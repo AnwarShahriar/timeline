30a6f41bfd535969ee089b2c918f90ff9e2dd815
anwarshahriar <anwarshahriar@gmail.com>
2017-11-02 07:47:08 +0600

    Simplified log fetching

--- commit-scrapper.js
+++ commit-scrapper.js
@@ -2,14 +2,12 @@ let fetchLogs = (cb, options) => {
     options = options || {}
     const repo = require('electron').remote.process.argv[2];
     const git = require('simple-git')(repo)
-    git.log((err, data) => {
+    let opts = options.reverse ? ["--reverse"] : [];
+    git.log(opts, (err, data) => {
         if (err) return cb(err, [])
 
         const container = document.getElementById('container')
         let logs = data.all
-        if (options.reverse) {
-            logs = logs.reverse();
-        }
         cb(err, logs)
     })
 }
